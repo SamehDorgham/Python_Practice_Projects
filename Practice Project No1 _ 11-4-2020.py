5317c752505b1540b354d47fb28e0dddc31a75bd
Sameh Dorgham <samehvb@gmail.com>
2020-04-26 22:10:52 +0200

    Update Multiplication Table Code

--- Practice Project No1 _ 11-4-2020.py
+++ Practice Project No1 _ 11-4-2020.py
@@ -80,7 +80,7 @@ class AllInOne_project1:
         last = int(input('enter the last number : '))
 
         for y in range(first,last+1):
-            for x in range(1,11):
+            for x in range(first,last+1):
                 print(y,'x',x,' = ', y * x)
             print('----------------------------')
 
